13ca555f0bd85ffd8e4e896a72f9c0e7a76651cc
Mike, Hua <mike.hua@sap.com>
2022-01-10 09:01:14 +0000

    Create Fiori Launchpad Sandbox

---
+++ app/mkfiori/webapp/Component.js
@@ -0,0 +1,9 @@
+sap.ui.define(["sap/fe/core/AppComponent"], function(AppComponent) {
+    'use strict';
+
+    return AppComponent.extend("mike.officesupplies.mkfiori.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
